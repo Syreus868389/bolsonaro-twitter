d03474deaca0f5e7bd90effe23eb56c097aa95a7
Syreus868389 <65856915+Syreus868389@users.noreply.github.com>
2020-12-30 22:39:45 +0100

    hide keys

--- bolsotwitter.py
+++ bolsotwitter.py
@@ -1,11 +1,11 @@
 import tweepy
 import pandas as pd
 
-consumer_key="x5ER0kkhEvbS8cJg8jDF2Z04T"
-consumer_secret="6WSZoraAH8CIMqkijT04OkdFSQ42LvoRSp7FFPmNTKT0h26fSR"
+consumer_key="**********"
+consumer_secret="*************"
 
-access_token="1224316478218014722-cTZc3P0nnSwlomGAUGN6qg3cU7kINQ"
-access_token_secret="7AUIeUtYsLINWzXb7tIAvOF9LCbWGC8fEhLyJkdMgRyof"
+access_token="**********"
+access_token_secret="**********"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -15,7 +15,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 id='128372940'
 
 text, rt, favs, date, link = ([] for i in range(5))
-tweets=tweepy.Cursor(api.user_timeline, user_id=id, tweet_mode='extended', include_rts=False).items(2)
+tweets=tweepy.Cursor(api.user_timeline, user_id=id, tweet_mode='extended', include_rts=False).items(3200)
 for status in tweets:
     text.append(status.full_text)
     rt.append(status.retweet_count)
